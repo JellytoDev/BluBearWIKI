df171eee17f7cf1567629055144c10c0ee341f71
origin <wjdwhdgus2222@naver.com>
2022-06-13 18:00:55 +0900

    Initial commit

---
+++ src/main/java/com/example/blubearwiki/BluBearWikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blubearwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluBearWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluBearWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blubearwiki/BluBearWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blubearwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluBearWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
