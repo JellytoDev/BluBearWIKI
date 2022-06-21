3cc1b04cbb0f14c8be1524867343beead3c1ab27
origin <wjdwhdgus2222@naver.com>
2022-06-21 17:20:54 +0900

    feat. document ckeditor 추가 및 save api 통신 ckeditor balloon block editor 추가 테스트(mark down) rest api 통신 (save 라우터, post 통신)

--- src/main/java/com/example/blubearwiki/controller/DocRestController.java
+++ src/main/java/com/example/blubearwiki/controller/DocRestController.java
@@ -2,12 +2,31 @@ package com.example.blubearwiki.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/rest/d")
 public class DocRestController {
 
+    @PostMapping(value = "/save", produces = "application/json")
+    public ResponseEntity save(@RequestBody Map<String,String> test) {
+
+        log.info("save test : " + test.toString());
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", "success");
+        result.put("data", "success");
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
 }
